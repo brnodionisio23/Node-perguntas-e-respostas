6a6faee0a3052efedfb4f5687e4cdfa4bd93acc6
Breno Dionisio <brnodionisio23@gmail.com>
2023-11-23 13:05:54 -0300

    Implementando o body-parser

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+// a biblioteca body-parser tem a finalidade de pegar os dados de uma requisição
+// http e converter para outros tipos como json
+const bodyParser = require("body-parser");
 
 const port = 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+// esta função tem a finalidade de manipular dados vindo de um fomulário codificado
+// em url e o objeto impede que dados complexo trafeguem por ele
+app.use(bodyParser.urlencoded({ extended: false }));
+// Permite manipular dados no formato json
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -12,6 +20,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntas", (req, res) => {
     res.render('perguntas')
+})
+
+app.post("/salvarPergunta", (req, res) => {
+    // apos o cliente enviar os dados eles serão capturados pelo bodyParser
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send();
 })
 
 app.listen(port, () => {
